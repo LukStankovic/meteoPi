accf2396aa436f338108ce1a217edfeab3b99dff
Lukáš Stankovič <lukasstankovic@gmail.com>
2016-10-07 21:53:29 +0200

    Added footer.php

--- history.php
+++ history.php
@@ -32,26 +32,4 @@ $minTotal = $temp->minTotalTemperature();
         <?php } ?>
     </ul>
 </div>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script>
-    $("document").ready(function () {
-
-        var show = 0;
-
-        $(".mobile--btn").click(function () {
-
-            if(show == 0) {
-                $("header nav ul").slideDown();
-                show = 1;
-            }
-            else {
-                $("header nav ul").slideUp();
-                show = 0;
-            }
-        });
-    });
-
-</script>
-</body>
-</html>
+<?php include_once "php/footer.php"; ?>
--- index.php
+++ index.php
@@ -189,26 +189,4 @@
         </div>
 
      </div>
-
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script>
-        $("document").ready(function () {
-
-            var show = 0;
-
-            $(".mobile--btn").click(function () {
-
-                if(show == 0) {
-                    $("header nav ul").slideDown();
-                    show = 1;
-                }
-                else {
-                    $("header nav ul").slideUp();
-                    show = 0;
-                }
-            });
-        });
-
-    </script>
-</body>
-</html>
+<?php include_once "php/footer.php"; ?>
